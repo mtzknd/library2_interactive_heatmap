c22e52671bab70c8bc09c1a082226a953f306d90
mtzknd <anne.zarda@hotmail.com>
2025-11-19 13:48:49 +0100

    updated mol pictures, updated column names

--- lib2_streamlit_app.py
+++ lib2_streamlit_app.py
@@ -15,7 +15,7 @@ df = load_data()
 
 @st.cache_data
 def pivot():
-    return df.pivot_table(values='res act', index = 'poolid_2', columns= 'poolid_1', sort=False)
+    return df.pivot_table(values='residual thrombin activity [%]', index = 'pool y', columns= 'pool x', sort=False)
 
 data = pivot()
 
@@ -24,7 +24,7 @@ cmap = px.colors.sequential.PuRd_r
 im = px.imshow(data, zmin=60, zmax = 100, color_continuous_scale= cmap)
 im.update_traces(hovertemplate = 'None', hoverinfo='skip')
 
-scat = im.add_trace(go.Scatter(mode ='markers', x = df['poolid_1'], y = df['poolid_2'], marker=dict(opacity=0)))
+scat = im.add_trace(go.Scatter(mode ='markers', x = df['pool x'], y = df['pool y'], marker=dict(opacity=0)))
 
 im.update_layout(height = 700)
 
@@ -46,7 +46,7 @@ if len(selected_points) != 0:
     # st.write(selected_points)
     x1 = selected_points[0]['x']
     y1 = selected_points[0]['y']
-    hoversrc = df[(df['poolid_1'] == x1) & (df['poolid_2'] == y1)]
+    hoversrc = df[(df['pool x'] == x1) & (df['pool y'] == y1)]
     if len(hoversrc) != 0:  
 
         with col2:
@@ -54,23 +54,23 @@ if len(selected_points) != 0:
             col3, space3, col4, space4, col5, space5, col6 = st.columns((1,0.5,1,0.5,1,0.5,1)) 
         with col3:    
             for i in range(1,4):
-                st.image("./mols/" + str(hoversrc['pep%s' % str(i)].values[0]) + '.png',
-                caption=hoversrc['pep%s' % str(i)].values[0])
+                st.image("./mols/" + str(hoversrc['monomer %s' % str(i)].values[0])[1:] + '.png',
+                caption=hoversrc['monomer %s' % str(i)].values[0])
         with col4:
             for i in range(4,7):
-                st.image("./mols/" + str(hoversrc['pep%s' % str(i)].values[0]) + '.png', 
-                         caption=hoversrc['pep%s' % str(i)].values[0])
+                st.image("./mols/" + str(hoversrc['monomer %s' % str(i)].values[0])[1:] + '.png', 
+                         caption=hoversrc['monomer %s' % str(i)].values[0])
         with col5:
             for i in range(7,10):
-                st.image("./mols/" + str(hoversrc['pep%s' % str(i)].values[0]) + '.png', 
-                         caption=hoversrc['pep%s' % str(i)].values[0])
+                st.image("./mols/" + str(hoversrc['monomer %s' % str(i)].values[0])[1:] + '.png', 
+                         caption=hoversrc['monomer %s' % str(i)].values[0])
         with col6:
             for i in range(10,13):
-                st.image("./mols/" + str(hoversrc['pep%s' % str(i)].values[0]) + '.png', 
-                         caption=hoversrc['pep%s' % str(i)].values[0])
+                st.image("./mols/" + str(hoversrc['monomer %s' % str(i)].values[0])[1:] + '.png', 
+                         caption=hoversrc['monomer %s' % str(i)].values[0])
 
         with col2:
             st.header('Raw Data')
-            st.write(hoversrc[['poolid_1', 'poolid_2', 'assayplate_well', 'time0', 'time1', 'time2', 'time3', 'time4', 'time5', 'time6', 'slopes', 'rvalue', 'res act']].set_index(['poolid_1', 'poolid_2']))
+            st.write(hoversrc[['pool x', 'pool y', 'assay plate well', 'time0 (0 s)','time1 (150 s)','time2 (300 s)','time3 (450 s)','time4 (600 s)','time5 (750 s)','time6 (900 s)','slope','rvalue','residual thrombin activity [%]']].set_index(['pool x', 'pool y']))
 
 
